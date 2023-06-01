796c52fe76a8ca5befda4d832a7759884ab15c1f
David Rebbe <drebbe@intrepidcs.com>
2023-06-01 15:52:35 -0400

    more changes

--- crates/libftd3xx/src/lib.rs
+++ crates/libftd3xx/src/lib.rs
@@ -1,6 +1,9 @@
 use libftd3xx_ffi::prelude::*;
 
-type Result<T> = std::result::Result<T, FT_STATUS>;
+pub mod types;
+
+use types::{Error, Result, Version};
+
 
 /// Returns the version of the underlying C library.
 ///
@@ -10,19 +13,64 @@ type Result<T> = std::result::Result<T, FT_STATUS>;
 /// # Example
 ///
 /// ```no_run
-/// use libftd2xx::library_version;
+/// use libftd3xx::library_version;
 ///
 /// let version = library_version()?;
-/// println!("libftd2xx C library version: {}", version);
-/// # Ok::<(), libftd2xx::FtStatus>(())
+/// println!("libftd3xx C library version: {}", version);
+/// # Ok::<(), libftd3xx::FtStatus>(())
 /// ```
-pub fn library_version() -> Result<u32> {
+pub fn library_version() -> Result<Version> {
     let mut version: u32 = 0;
     //trace!("FT_GetLibraryVersion(_)");
-    let status: FT_STATUS = unsafe { FT_GetLibraryVersion(&mut version) };
-    if status == FT_OK as FT_STATUS {
-        return Ok(version);
-    } else {
-        return Err(status);
+    let status = unsafe { FT_Status::try_from(FT_GetLibraryVersion(&mut version)) }?;
+    if status == FT_OK {
+        return Ok(Version::with_raw(version));
+    }
+    else {
+        return Err(Error::APIError(status));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_library_version() {
+        let result = library_version();
+        //let expected_version: u32 = 0x0;
+        cfg_if::cfg_if! {
+            if #[cfg(all(target_os = "linux", target_arch = "x86_64"))] {
+                // version "1.0.5" is represented as 0x010005
+                // let expected_version = 0x01_00_05;
+                // for some reason 1.0.5 reports as 0x1000016
+                let expected_version = 0x1_00_00_16;
+            } else if #[cfg(all(target_os = "linux", target_arch = "x86"))] {
+                // version "1.0.5" is represented as 0x010005
+                let expected_version = 0x01_00_05;
+            } else if #[cfg(all(target_os = "windows", target_arch = "x86_64"))] {
+                // version "1.3.0.4" is represented as 0x1030004
+                let expected_version = 0x1_03_00_04;
+            } else if #[cfg(all(target_os = "windows", target_arch = "x86"))] {
+                let expected_version = 0x1_03_00_04;
+            } else if #[cfg(all(target_os = "linux", target_arch = "arm"))] {
+                // version "1.0.5" is represented as 0x010005
+                let expected_version = 0x01_00_05;
+            } else if #[cfg(all(target_os = "linux", target_arch = "aarch64"))] {
+                // version "1.0.5" is represented as 0x010005
+                let expected_version = 0x01_00_05;
+            } else if #[cfg(all(target_os = "macos", target_arch = "x86_64"))] {
+                // version "1.0.5" is represented as 0x010005
+                let expected_version = 0x01_00_05;
+            } else if #[cfg(all(target_os = "macos", target_arch = "aarch64"))] {
+                // version "1.0.5" is represented as 0x010005
+                let expected_version = 0x01_00_05;
+            } else {
+                std::compile_error!("pre-generated bindings are not avaliable for your target");
+            }
+        };
+        let expected_version = Version::with_raw(expected_version);
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap(), expected_version);
     }
 }
---
+++ crates/libftd3xx/src/types.rs
@@ -0,0 +1,137 @@
+use libftd3xx_ffi::prelude::*;
+use core::fmt;
+
+/// Errors associated with this library
+#[derive(Debug)]
+pub enum Error {
+    /// Low level API error directly from the FTD3xx Library
+    APIError(FT_Status),
+    /// Low level Critical API error, this is something that would normally "panic"
+    CriticalAPIError(String),
+}
+
+impl std::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Self::APIError(s) => write!(f, "API Error: {:#?}", s),
+            Self::CriticalAPIError(s) => write!(f, "Critical API Error: {:#?}", s),
+        }
+    }
+}
+
+impl From<&str> for Error {
+    fn from(value: &str) -> Self {
+        Self::CriticalAPIError(value.to_string())
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// D3XX version structure.
+///
+/// A version number contains a major version number, minor version and build/SVN version.
+/// Byte 0 and 1 (least significant) holds the build/SVN version. Byte 2 holds the minor
+/// version. Byte 3 holds the major version.
+///
+/// This is returned by [`library_version`] and [`driver_version`].
+///
+/// [`library_version`]: crate::library_version
+/// [`driver_version`]: crate::FtdiCommon::driver_version
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Version {
+    /// Major version.
+    pub major: u8,
+    /// Minor version.
+    pub minor: u8,
+    /// Build number.
+    pub build: u16,
+}
+
+#[cfg(test)]
+mod version {
+    use super::*;
+
+    macro_rules! case {
+        ($name:ident, ($a:expr, $b:expr, $c:expr), ($d:expr, $e:expr, $f:expr)) => {
+            #[test]
+            fn $name() {
+                let big = Version::new($a, $b, $c);
+                let little = Version::new($d, $e, $f);
+                assert!(big > little);
+                assert!(little < big);
+            }
+        };
+    }
+
+    case!(case_1, (0, 0, 1), (0, 0, 0));
+    case!(case_2, (0, 1, 0), (0, 0, 0));
+    case!(case_3, (1, 0, 0), (0, 0, 0));
+    case!(case_4, (2, 2, 2), (1, 1, 1));
+    case!(case_5, (255, 255, 255), (254, 255, 255));
+    case!(case_6, (1, 0, 0), (0, 255, 255));
+    case!(case_7, (13, 255, 0), (13, 254, 255));
+
+    #[test]
+    fn display() {
+        assert_eq!(
+            format!("{}", Version::with_raw(0x00030115)),
+            String::from("3.1.15")
+        )
+    }
+}
+
+impl fmt::Display for Version {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}.{}.{}", self.major, self.minor, self.build)
+    }
+}
+
+impl Version {
+    /// Create a new version structure from decimal values.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use libftd2xx::Version;
+    ///
+    /// let version = Version::new(3, 1, 15);
+    /// assert_eq!(
+    ///     version,
+    ///     Version {
+    ///         major: 3,
+    ///         minor: 1,
+    ///         build: 15
+    ///     }
+    /// );
+    /// ```
+    pub const fn new(major: u8, minor: u8, build: u16) -> Version {
+        Version {
+            major,
+            minor,
+            build,
+        }
+    }
+
+    /// Create a new version structure from the raw C-API value.
+    ///
+    /// Byte 0 and 1 (least significant) holds the build/SVN version. Byte 2 holds the minor
+    /// version. Byte 3 holds the major version.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use libftd2xx::Version;
+    ///
+    /// let version = Version::with_raw(0x00030115);
+    /// assert_eq!(version, Version::new(3, 1, 15));
+    /// ```
+    pub fn with_raw(value: u32) -> Version {
+        Version::new(
+            ((value >> 24) & 0xFF) as u8,
+            ((value >> 16) & 0xFF) as u8,
+            (value & 0xFFFF) as u16,
+        )
+    }
+}
