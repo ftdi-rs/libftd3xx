6fb108659074ca929c0ea64b87ce7b7aaaeaf608
David Rebbe <drebbe@intrepidcs.com>
2023-06-15 11:07:10 -0400

    updated unit test

--- crates/libftd3xx-py/tests/test_version.py
+++ crates/libftd3xx-py/tests/test_version.py
@@ -1,6 +1,10 @@
 import unittest
 import libftd3xx as ftd3xx
 
+# import os
+# input(f"OS PID: {os.getpid()}")
+
+
 class LibraryVersionTestCase(unittest.TestCase):
     def setUp(self):
         pass
@@ -8,20 +12,18 @@ class LibraryVersionTestCase(unittest.TestCase):
     def test_get_library_version(self):
         _ = ftd3xx.get_library_version()
 
+
 class DriverVersionTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_get_driver_version(self):
-        import os
-        input(f"OS PID: {os.getpid()}")
-
         self.assertRaises(TypeError, ftd3xx.get_driver_version, (None,))
 
         device_count = ftd3xx.create_device_info_list()
-        device_info = ftd3xx.get_device_info_list(device_count)
         if device_count > 0:
-            #handle = ftd3xx.create_by_serial_number(device_info[0].SerialNumber)
+            # device_info = ftd3xx.get_device_info_list(device_count)
+            # handle = ftd3xx.create_by_serial_number(device_info[0].SerialNumber)
             handle = ftd3xx.create_by_index(0)
             _ = ftd3xx.get_driver_version(handle)
             print(f"Driver Version: {_}")
