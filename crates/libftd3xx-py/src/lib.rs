01c73924cfa1ac90bed6a72ce2d7fc26e2340772
David Rebbe <drebbe@intrepidcs.com>
2023-06-15 11:07:37 -0400

    added get/set_chip_configuration

--- crates/libftd3xx-py/src/lib.rs
+++ crates/libftd3xx-py/src/lib.rs
@@ -1,9 +1,9 @@
-use pyo3::prelude::*;
 use ::libftd3xx as ftd3xx;
+use pyo3::prelude::*;
 
 pub mod types;
 
-use types::{FtHandle, FtDeviceListInfoNode, Result, Version, FtException};
+use types::{Ft60xConfiguration, FtDeviceListInfoNode, FtException, FtHandle, Result, Version};
 
 #[pyfunction]
 fn get_library_version() -> Result<Version> {
@@ -25,14 +25,14 @@ fn create_device_info_list() -> Result<u32> {
     Ok(ftd3xx::functions::create_device_info_list()?)
 }
 
-
 #[pyfunction]
 fn get_device_info_list(device_count: u32) -> Result<Vec<FtDeviceListInfoNode>> {
     let mut device_count = device_count;
     let device_info_list = ftd3xx::functions::get_device_info_list(&mut device_count)?;
-    Ok(
-        device_info_list.into_iter().map(|x| FtDeviceListInfoNode::from(x)).collect(),
-    )
+    Ok(device_info_list
+        .into_iter()
+        .map(|x| FtDeviceListInfoNode::from(x))
+        .collect())
 }
 
 #[pyfunction]
@@ -42,12 +42,16 @@ fn create_by_index(index: u32) -> Result<FtHandle> {
 
 #[pyfunction]
 fn create_by_description(description: String) -> Result<FtHandle> {
-    Ok(FtHandle::from(ftd3xx::functions::create_by_description(description)?))
+    Ok(FtHandle::from(ftd3xx::functions::create_by_description(
+        description,
+    )?))
 }
 
 #[pyfunction]
 fn create_by_serial_number(serial_number: String) -> Result<FtHandle> {
-    Ok(FtHandle::from(ftd3xx::functions::create_by_serial_number(serial_number)?))
+    Ok(FtHandle::from(ftd3xx::functions::create_by_serial_number(
+        serial_number,
+    )?))
 }
 
 #[pyfunction]
@@ -55,10 +59,29 @@ fn close(handle: &FtHandle) -> Result<()> {
     Ok(ftd3xx::functions::close(*handle.0.lock().unwrap())?)
 }
 
+#[pyfunction]
+fn get_chip_configuration(handle: &FtHandle) -> Result<Ft60xConfiguration> {
+    Ok(Ft60xConfiguration::from(
+        ftd3xx::functions::get_chip_configuration(*handle.0.lock().unwrap())?,
+    ))
+}
+
+#[pyfunction]
+fn set_chip_configuration(handle: &FtHandle, config: Option<&Ft60xConfiguration>) -> Result<()> {
+    let result = match &config {
+        Some(c) => ftd3xx::functions::set_chip_configuration(
+            *handle.0.lock().unwrap(),
+            Some(*c.0.lock().unwrap()),
+        )?,
+        None => ftd3xx::functions::set_chip_configuration(*handle.0.lock().unwrap(), None)?,
+    };
+    Ok(result)
+}
+
 #[pymodule]
 fn libftd3xx(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add("FtException", py.get_type::<FtException>())?;
-    
+
     m.add_function(wrap_pyfunction!(get_library_version, m)?)?;
     m.add_function(wrap_pyfunction!(get_driver_version, m)?)?;
 
@@ -69,5 +92,9 @@ fn libftd3xx(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(create_by_description, m)?)?;
     m.add_function(wrap_pyfunction!(create_by_serial_number, m)?)?;
     m.add_function(wrap_pyfunction!(close, m)?)?;
+
+    m.add_function(wrap_pyfunction!(get_chip_configuration, m)?)?;
+    m.add_function(wrap_pyfunction!(set_chip_configuration, m)?)?;
+
     Ok(())
 }
--- crates/libftd3xx-py/src/types.rs
+++ crates/libftd3xx-py/src/types.rs
@@ -1,5 +1,7 @@
 use libftd3xx_ffi::DWORD;
+use libftd3xx_ffi::UCHAR;
 use libftd3xx_ffi::ULONG;
+use libftd3xx_ffi::USHORT;
 use pyo3::create_exception;
 use pyo3::exceptions::PyRuntimeError;
 use pyo3::prelude::*;
@@ -9,7 +11,7 @@ use std::sync::{Arc, Mutex};
 
 use ::libftd3xx as ftd3xx;
 use ftd3xx::types::Error as FtError;
-use libftd3xx_ffi::{prelude::*, FT_DEVICE_LIST_INFO_NODE};
+use libftd3xx_ffi::{prelude::*, FT_DEVICE_LIST_INFO_NODE, FT_60XCONFIGURATION};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
@@ -201,4 +203,209 @@ impl FtDeviceListInfoNode {
         let info = *self.0.lock().unwrap();
         Ok(FtHandle::from(info.ftHandle))
     }
+}
+
+define_basic_py_object_no_new!(Ft60xConfiguration, FT_60XCONFIGURATION);
+
+impl Ft60xConfiguration {
+    fn new() -> Self {
+        Self {
+            0: Arc::new(Mutex::new(FT_60XCONFIGURATION::default())),
+        }
+    }
+
+    pub fn from(data: FT_60XCONFIGURATION) -> Self {
+        Self {
+            0: Arc::new(Mutex::new(data)),
+        }
+    }
+}
+
+#[pymethods]
+impl Ft60xConfiguration {
+    #[new]
+    fn py_new() -> Self {
+        Self::new()
+    }
+
+    #[getter(VendorID)]
+    fn get_vendor_id(&self) -> PyResult<USHORT> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.VendorID)
+    }
+    #[setter(VendorID)]
+    fn set_vendor_id(&mut self, value: USHORT) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.VendorID = value;
+        Ok(())
+    }
+
+    #[getter(ProductID)]
+    fn get_product_id(&self) -> PyResult<USHORT> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.ProductID)
+    }
+    #[setter(ProductID)]
+    fn set_product_id(&mut self, value: USHORT) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.ProductID = value;
+        Ok(())
+    }
+
+    // StringDescriptors: [UCHAR; 128usize],
+    #[getter(StringDescriptors)]
+    fn get_string_descriptors(&self) -> PyResult<[UCHAR; 128usize]> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.StringDescriptors)
+    }
+    #[setter(StringDescriptors)]
+    fn set_string_descriptors(&mut self, value: [UCHAR; 128usize]) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.StringDescriptors = value;
+        Ok(())
+    }
+
+    #[getter(bInterval)]
+    fn get_b_interval(&self) -> PyResult<UCHAR> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.bInterval)
+    }
+    #[setter(bInterval)]
+    fn set_b_interval(&mut self, value: UCHAR) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.bInterval = value;
+        Ok(())
+    }
+
+    #[getter(PowerAttributes)]
+    fn get_power_attributes(&self) -> PyResult<UCHAR> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.PowerAttributes)
+    }
+    #[setter(PowerAttributes)]
+    fn set_power_attributes(&mut self, value: UCHAR) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.PowerAttributes = value;
+        Ok(())
+    }
+
+    #[getter(PowerConsumption)]
+    fn get_power_consumption(&self) -> PyResult<USHORT> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.PowerConsumption)
+    }
+    #[setter(PowerConsumption)]
+    fn set_power_consumption(&mut self, value: USHORT) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.PowerConsumption = value;
+        Ok(())
+    }
+
+    #[getter(Reserved2)]
+    fn get_reserved2(&self) -> PyResult<UCHAR> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.Reserved2)
+    }
+    #[setter(Reserved2)]
+    fn set_reserved2(&mut self, value: UCHAR) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.Reserved2 = value;
+        Ok(())
+    }
+
+    #[getter(FIFOClock)]
+    fn get_fifo_clock(&self) -> PyResult<UCHAR> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.FIFOClock)
+    }
+    #[setter(FIFOClock)]
+    fn set_fifo_clock(&mut self, value: UCHAR) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.FIFOClock = value;
+        Ok(())
+    }
+
+    #[getter(FIFOMode)]
+    fn get_fifo_mode(&self) -> PyResult<UCHAR> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.FIFOMode)
+    }
+    #[setter(FIFOMode)]
+    fn set_fifo_mode(&mut self, value: UCHAR) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.FIFOMode = value;
+        Ok(())
+    }
+
+    #[getter(ChannelConfig)]
+    fn get_channel_config(&self) -> PyResult<UCHAR> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.ChannelConfig)
+    }
+    #[setter(ChannelConfig)]
+    fn set_channel_config(&mut self, value: UCHAR) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.ChannelConfig = value;
+        Ok(())
+    }
+
+    #[getter(OptionalFeatureSupport)]
+    fn get_optional_feature_support(&self) -> PyResult<USHORT> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.OptionalFeatureSupport)
+    }
+    #[setter(OptionalFeatureSupport)]
+    fn set_optional_feature_support(&mut self, value: USHORT) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.OptionalFeatureSupport = value;
+        Ok(())
+    }
+
+    #[getter(BatteryChargingGPIOConfig)]
+    fn get_battery_charging_gpio_config(&self) -> PyResult<UCHAR> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.BatteryChargingGPIOConfig)
+    }
+    #[setter(BatteryChargingGPIOConfig)]
+    fn set_battery_charging_gpio_config(&mut self, value: UCHAR) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.BatteryChargingGPIOConfig = value;
+        Ok(())
+    }
+
+    #[getter(FlashEEPROMDetection)]
+    fn get_flash_eeprom_detection(&self) -> PyResult<UCHAR> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.FlashEEPROMDetection)
+    }
+    #[setter(FlashEEPROMDetection)]
+    fn set_flash_eeprom_detection(&mut self, value: UCHAR) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.FlashEEPROMDetection = value;
+        Ok(())
+    }
+
+    #[getter(MSIO_Control)]
+    fn get_msio_control(&self) -> PyResult<ULONG> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.MSIO_Control)
+    }
+    #[setter(MSIO_Control)]
+    fn set_msio_control(&mut self, value: ULONG) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.MSIO_Control = value;
+        Ok(())
+    }
+
+    #[getter(GPIO_Control)]
+    fn get_gpio_control(&self) -> PyResult<ULONG> {
+        let config = *self.0.lock().unwrap();
+        Ok(config.GPIO_Control)
+    }
+    #[setter(GPIO_Control)]
+    fn set_gpio_control(&mut self, value: ULONG) -> PyResult<()> {
+        let mut config = *self.0.lock().unwrap();
+        config.GPIO_Control = value;
+        Ok(())
+    }    
 }
